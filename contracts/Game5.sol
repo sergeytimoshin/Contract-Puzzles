a8886281c6ba7ec5461d585daa4cb435511a98f7
Sergey Timoshin <timoshin.sergey@gmail.com>
2022-05-25 21:10:59 +0300

    Log bytes20

--- contracts/Game5.sol
+++ contracts/Game5.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.7.0;
+import "hardhat/console.sol";
 
 contract Game5 {
   bool public isWon;
@@ -7,6 +8,7 @@ contract Game5 {
   address threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
 
   function win() external {
+    console.logBytes20(bytes20(threshold));
     require(bytes20(msg.sender) < bytes20(threshold), "Nope. Try again!");
 
     isWon = true;
